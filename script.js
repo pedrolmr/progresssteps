a86a489a535946f0be5a85281be94cfe55292622
pedrolmr <pedroluismontesinosrivera@gmail.com>
2022-02-11 17:25:40 -0500

    progress bar works

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ next.addEventListener("click", () => {
   if (currentActive > circles.length) {
     currentActive = circles.length;
   }
-  console.log(currentActive);
+  update();
 });
 
 prev.addEventListener("click", () => {
@@ -19,4 +19,20 @@ prev.addEventListener("click", () => {
     currentActive = 1;
     1;
   }
+  update();
 });
+
+function update() {
+  circles.forEach((circle, idx) => {
+    if (idx < currentActive) {
+      circle.classList.add("active");
+    } else {
+      circle.classList.remove("active");
+    }
+  });
+
+  const actives = document.querySelectorAll(".active");
+  console.log(actives.length, circles.length);
+  progress.style.width =
+    ((actives.length - 1) / (circles.length - 1)) * 100 + "%";
+}
